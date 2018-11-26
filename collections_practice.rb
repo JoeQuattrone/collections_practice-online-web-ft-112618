4c8506975cec97961acd7253908525dc4efb1c79
Joe Quattrone <joequattrone5@gmail.com>
2018-11-26 16:52:57 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -43,5 +43,8 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect
+  array.each_with_index.collect do |element, index|
+    unless index == 1
+    element.push("s")
+  end
 end
